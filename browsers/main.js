a4f1abb47b9fe81a0065e9a74ca66e471a280989
Taukom <153712618+Taukom@users.noreply.github.com>
2023-12-13 11:24:33 +0400

    Add files via upload

---
+++ browsers/main.js
@@ -0,0 +1,34 @@
+// const button = document.querySelector("#button")
+// const es = document.querySelector("#element-1")
+
+// button.addEventListener('click', function(){
+//     es.style.display = (es.style.display === 'none') ? 'block' : 'none';
+// })
+
+
+function abOn() {
+    document.getElementById('ab').classList.add("active");
+}
+function abOff() {
+    document.getElementById('ab').classList.remove("active");
+}
+function gaOn() {
+    document.getElementById('ga').classList.add("active");
+}
+function gaOff() {
+    document.getElementById('ga').classList.remove("active");
+}
+function piOn() {
+    document.getElementById('pi').classList.add("active");
+}
+function piOff() {
+    document.getElementById('pi').classList.remove("active");
+}
+function hoOn() {
+    document.getElementById('ho').classList.add("active");
+}
+function hoOff() {
+    document.getElementById('ho').classList.remove("active");
+}
+
+
